5401e06eea58bda040f18acecba802729aa9bb23
lanou <2124974816@qq.com>
2016-07-12 10:47:46 +0800

    ooo

---
+++ 123456/123456/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  123456
+//
+//  Created by lanou on 16/7/12.
+//  Copyright © 2016年 lanon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"123456");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
